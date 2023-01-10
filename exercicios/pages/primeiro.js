5d2772ad1711fca9caa3f6b5a28f354f537570d2
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-01-09 21:57:30 -0300

    implementing exercicios route

---
+++ exercicios/pages/primeiro.js
@@ -0,0 +1,12 @@
+/**
+ * O Next utilza o node
+ * 
+ * Quando criamos arquivos dentro da pasta pages, automaticamente é criado a rota.
+ * Portanto nesse caso teremo "url|localhost:3000/primeiro"
+ */
+
+function primeiro() {
+    return "Primeiro alterado"
+}
+
+export default primeiro;
