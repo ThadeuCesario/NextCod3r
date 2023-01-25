fab796364ecfb8027cac5a62d034049f8112d007
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-01-25 19:50:25 -0300

    Door component - stage #01

---
+++ portas/components/Porta.jsx
@@ -0,0 +1,11 @@
+import styles from "../src/styles/Porta.module.css";
+
+function Porta(props) {
+    return (
+        <div className={styles.porta}>
+            <div className={styles.numero}>3</div>
+        </div>
+    )
+}
+
+export default Porta;
--- portas/src/pages/index.jsx
+++ portas/src/pages/index.jsx
@@ -1,9 +1,10 @@
+import Porta from "components/Porta";
 import Presente from "components/Presente";
 
 export default function Home() {
   return (
     <div>
-      <Presente />
+      <Porta />
     </div>
   )
 }
