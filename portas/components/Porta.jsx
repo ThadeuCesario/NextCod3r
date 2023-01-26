324fc305b61af0fb1a004665dc4df2cf84bfb76b
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-01-26 12:29:38 -0300

    implementing styles to porta component

--- portas/components/Porta.jsx
+++ portas/components/Porta.jsx
@@ -1,11 +1,18 @@
 import styles from "../src/styles/Porta.module.css";
 
-function Porta(props) {
-    return (
+function Porta({selecionada}) {
+
+  return (
+    <div className={styles.area}>
+      <div className={`${styles.estrutura} ${selecionada ? styles.selecionada : ''}`}>
         <div className={styles.porta}>
-            <div className={styles.numero}>3</div>
+          <div className={styles.numero}>3</div>
+          <div className={styles.macaneta} />
         </div>
-    )
+      </div>
+      <div className={styles.chao}></div>
+    </div>
+  );
 }
 
 export default Porta;
--- portas/src/pages/index.jsx
+++ portas/src/pages/index.jsx
@@ -3,7 +3,8 @@ import Presente from "components/Presente";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{display: 'flex'}}>
+      <Porta selecionada />
       <Porta />
     </div>
   )
