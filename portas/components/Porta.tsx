856b1c94c122c4014cd92357fda43c115a0b041c
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-01-28 21:03:20 -0300

    integrating typescript in project

--- portas/components/Porta.tsx
+++ portas/components/Porta.tsx
@@ -1,12 +1,18 @@
 import styles from "../src/styles/Porta.module.css";
+import PortaModel from "../model/porta";
 
-function Porta({selecionada}) {
+interface PortaProps {
+  porta: PortaModel
+}
+
+function Porta(props: PortaProps) {
+  const { porta: {selecionada, numero} } = props;
 
   return (
     <div className={styles.area}>
       <div className={`${styles.estrutura} ${selecionada ? styles.selecionada : ''}`}>
         <div className={styles.porta}>
-          <div className={styles.numero}>3</div>
+          <div className={styles.numero}>{numero}</div>
           <div className={styles.macaneta} />
         </div>
       </div>
--- portas/src/pages/index.jsx
+++ portas/src/pages/index.jsx
@@ -1,11 +1,14 @@
 import Porta from "components/Porta";
 import Presente from "components/Presente";
+import { useState } from "react";
+import PortaModel from "../../model/porta";
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1));
+
   return (
     <div style={{display: 'flex'}}>
-      <Porta selecionada />
-      <Porta />
+      <Porta porta={p1} />
     </div>
   )
 }
