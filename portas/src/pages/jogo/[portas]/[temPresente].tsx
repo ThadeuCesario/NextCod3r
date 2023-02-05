7bf30e22e0903b7d58d226d17f44090a958e3839
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-02-05 19:01:37 -0300

    inserting validation in door quantity

--- portas/src/pages/jogo/[portas]/[temPresente].tsx
+++ portas/src/pages/jogo/[portas]/[temPresente].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -9,7 +10,20 @@ import { useRouter } from "next/router";
 
 function jogo() {
   const router = useRouter();
+
+  const [valido, setValido] = useState(false);
   const [portas, setPortas] = useState([]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+
+    const qtdePortasValidas = portas >= 3 && portas <= 100;
+    const temPresenteValido =  temPresente >= 1 && temPresente <= portas; 
+
+    setValido(qtdePortasValidas && temPresenteValido);
+    
+  }, [portas]);
 
   useEffect(() => {
     const portas = +router.query.portas;
@@ -31,7 +45,7 @@ function jogo() {
     return (
         <div className={styles.jogo}>
             <div className={styles.portas}>
-                {renderizarPortas()}
+                {valido ? renderizarPortas() : <h2>Valores inválidos</h2>}
             </div>
             <div className={styles.botoes}>
               <Link href="/">
