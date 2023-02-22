fc68d4fc917a0b861811ac53efbde4d46247687a
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-02-22 12:04:16 -0300

    getStaticProps function

--- renderizacao/src/pages/estatico4.jsx
+++ renderizacao/src/pages/estatico4.jsx
@@ -1,4 +1,6 @@
 export async function getStaticProps() {
+    console.log('[Server] gerando props para o componente...');
+
     const response = await fetch('http://localhost:3000/api/produtos'); // returns a promise
     const produtos = await response.json(); // returns a promise
 
