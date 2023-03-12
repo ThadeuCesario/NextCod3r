8ad64ab9e8499d1ad785bd209d864bb1b7f0dd7e
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-03-12 11:39:21 -0300

    estatico 04 class

---
+++ renderizacao/src/pages/estatico4.jsx
@@ -0,0 +1,29 @@
+export async function getStaticProps() {
+    const response = await fetch('http://localhost:3000/api/produtos'); // returns a promise
+    const produtos = await response.json(); // returns a promise
+
+    return {
+        props: {
+            produtos
+        }
+    }
+}
+
+function estatico3(props) {
+    function renderizarProdutos() {
+        return props.produtos.map(produto => (
+            <li key={produto.id}>{produto.nome} - preço R${produto.preco}</li>
+        )) 
+    }
+
+    return (
+        <div>
+            <h1>Estático #04</h1>
+            <ul>
+                {renderizarProdutos()}
+            </ul>
+        </div>
+    )
+}
+
+export default estatico3;
