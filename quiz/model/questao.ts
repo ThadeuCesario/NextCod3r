234a9204ce00332eaedfc76bedff4c32e3fce8ab
Thadeu Cesario <thadeu.cesario@icloud.com>
2023-10-13 15:48:00 -0300

    init commit quiz project

---
+++ quiz/model/questao.ts
@@ -0,0 +1,40 @@
+import RespostaModel from "./resposta";
+
+export default class QuestaoModel {
+    #id: number;
+    #enunciado: string;
+    #respostas: RespostaModel[];
+    #acertou: boolean;
+    // #respondida: boolean;
+
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou: boolean = false) {
+        this.#id = id;
+        this.#enunciado = enunciado;
+        this.#respostas = respostas;
+        this.#acertou = acertou;
+        // this.#respondida = false;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get enunciado() {
+        return this.#enunciado;
+    }
+
+    get respostas() {
+        return this.#respostas;
+    }
+
+    get acertou() {
+        return this.#acertou;
+    }
+
+    get respondida() {
+        this.#respostas.forEach(resposta => {
+            if(resposta.revelada) return true;
+        })
+        return false;
+    }
+}
